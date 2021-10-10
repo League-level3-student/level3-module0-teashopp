34c451848ebfc2076b6d5de553177b5c5f868467
ethan <ethan.yuan@gmail.com>
2021-10-10 14:28:19 -0700

    10/10

--- src/_03_Intro_to_2D_Arrays/_01_IntroTo2DArrays.java
+++ src/_03_Intro_to_2D_Arrays/_01_IntroTo2DArrays.java
@@ -1,21 +1,35 @@
 package _03_Intro_to_2D_Arrays;
 
+import java.util.Random;
+
 public class _01_IntroTo2DArrays {
 
-    public static void main(String[] args) {
-        // 1. declare a 2D array of integers with 3, 1D arrays that each hold
-        //    3 integers
+	public static void main(String[] args) {
+		// 1. declare a 2D array of integers with 3, 1D arrays that each hold
+		// 3 integers
+		int[][] array2D = new int[3][3];
 
-        // 2. use two for loops to set all the elements in the 2D array to
-        //    a different integer
-        //    BE SURE TO USE THE ARRAY'S length VARIABLE
-        
-        // 3. print the third element from the second 1D array (2DArray[1][2])
+		// 2. use two for loops to set all the elements in the 2D array to
+		// a different integer
+		// BE SURE TO USE THE ARRAY'S length VARIABLE
+		for (int i = 0; i < array2D.length; i++) {
+			for (int j = 0; j < array2D.length; j++) {
+				Random randrand = new Random();
+				int randint = randrand.nextInt(20 - 1) + 1;
+				array2D[i][j] = randint;
+			}
+		}
 
-        // 4. set the third element from the second 1D array to a different
-        //    value
+		// 3. print the third element from the second 1D array (2DArray[1][2])
+		System.out.println("The third element from the second array is: " + array2D[2][3]);
 
-        // 5. print the element again and verify the value has changed
+		// 4. set the third element from the second 1D array to a different
+		// value
+		Random randrand = new Random();
+		int randint = randrand.nextInt(20 - 1) + 1;
+		array2D[2][3] = randint;
 
-    }
+		// 5. print the element again and verify the value has changed
+		System.out.println("The new third element from the second array is: " + array2D[2][3]);
+	}
 }
